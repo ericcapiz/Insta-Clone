4b4da390238917482b762b5d8a1f4a7a1a7fa64d
eric capiz <ericcapiz@gmail.com>
2024-02-28 16:37:57 -0600

    check for existing username

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,30 +1,46 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/Comments/Navbar/Navbar";
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const renderSidebar = pathname !== "/auth" && user;
+  const renderNavbar = !user && !loading && pathname !== "/auth";
+  const authenticatedUser = !user && loading;
+
+  if (authenticatedUser) return <PageLayoutSpinner />;
+
   return (
-    <div>
-      <Flex>
-        {renderSidebar ? (
-          <Box w={{ base: "70px", md: "240px" }}>
-            <Sidebar />
-          </Box>
-        ) : null}
-        <Box
-          flex={1}
-          w={{ base: "calc(100% - 70%)", md: "calc(100% - 240px)" }}
-        >
-          {children}
+    <Flex flexDirection={renderNavbar ? "column" : "row"}>
+      {renderSidebar ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <Sidebar />
         </Box>
-      </Flex>
-    </div>
+      ) : null}
+      {renderNavbar ? <Navbar /> : null}
+      <Box
+        flex={1}
+        w={{ base: "calc(100% - 70%)", md: "calc(100% - 240px)" }}
+        mx={"auto"}
+      >
+        {children}
+      </Box>
+    </Flex>
   );
 };
 
 export default PageLayout;
+const PageLayoutSpinner = () => {
+  <Flex
+    flexDirection={"column"}
+    h={"100vh"}
+    alignItems={"center"}
+    justifyContent={"center"}
+  >
+    <Spinner size={"xl"} />
+  </Flex>;
+};
