f4aa891d243fa324b47e703c1c5633f49c1923f4
eric capiz <ericcapiz@gmail.com>
2024-02-29 20:55:03 -0600

    bug: replaced 'profile' in sidebar menu w/username of user

--- src/components/Sidebar/ProfileLink.jsx
+++ src/components/Sidebar/ProfileLink.jsx
@@ -27,7 +27,7 @@ const ProfileLink = () => {
         justifyContent={{ base: "center", md: "flex-start" }}
       >
         <Avatar size={"sm"} src={authUser?.profilePicURL || ""} />
-        <Box display={{ base: "none", md: "block" }}>Profile</Box>
+        <Box display={{ base: "none", md: "block" }}>{authUser?.username}</Box>
       </Link>
     </Tooltip>
   );
